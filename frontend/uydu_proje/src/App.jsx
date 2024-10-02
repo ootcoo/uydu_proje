165c5b75f75676975e97e343d85a7e3ef982c717
Tugba <tgbcengizhan@hotmail.com>
2024-10-02 16:05:28 +0300

    filtre arama barı

--- frontend/uydu_proje/src/App.jsx
+++ frontend/uydu_proje/src/App.jsx
@@ -5,15 +5,17 @@ import Header from './Header.jsx'
 import Eski_goruntu from './Eski_goruntu.jsx'
 import Yeni_goruntu from './Yeni_goruntu.jsx'
 import Sonuclar from './sonuclar.jsx'
+import { useState } from 'react';
+
 
 
 
 
 function App() {
+  const [marker, setMarker] = useState(null);
+  const [search, setSearch] = useState("");
 
-
-
-
+  
 
   return (
 
@@ -21,9 +23,8 @@ function App() {
       <Header />
       <div style={{ display: 'flex', flex: 1, flexDirection: 'row' }}>
 
-        <Filtreler />
-
-        <Map />
+        <Filtreler marker={marker} setMarker={setMarker} search={search} setSearch={setSearch} />
+        <Map marker={marker} setMarker={setMarker} search={search} setSearch={setSearch}/>
       </div>
       <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
         <button className='buttonum'>Göster</button>
--- frontend/uydu_proje/src/Filtreler.jsx
+++ frontend/uydu_proje/src/Filtreler.jsx
@@ -1,13 +1,35 @@
 
 import React from 'react';
+import axios from 'axios';
 
 
-function Filtreler(){
 
-  return(
-<div className="filtreler" style={{flex:2}}>
-  <h1>Filtreler</h1>
-</div>
+
+function Filtreler({ marker, setMarker, search, setSearch }) {
+  const HandleSearch = async () => {
+    try {
+      const response = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${search}`);
+      if (response.data.length > 0) {
+        const { lat, lon } = response.data[0];
+        setMarker({ lat: parseFloat(lat), lng: parseFloat(lon) });
+      }
+       else {
+        alert("Aradığınız konum bulunamadı");
+      }
+    }
+    catch (error) {
+      console.error("Error searching location", error);
+    }
+  };
+  
+  return (
+    <div className="filtreler" style={{ flex: 2 }}>
+      <h1>Filtreler</h1>
+
+      <input type="text" value={search} placeholder="Arama yapın" onChange={(e) => { setSearch(e.target.value) }} style={{scale: '120%'}} />
+    <button className="buttonum" onClick={HandleSearch}>Ara</button>
+
+    </div>
 
   )
 }
